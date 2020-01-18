e3a80aad6d32037d8f5c54a7768ca6b0bcf5a975
Gabriel lima <gabrielveigalima@icloud.com>
2020-01-18 17:19:36 -0300

    arquivo de conexao

---
+++ main.py
@@ -0,0 +1,30 @@
+import cx_Oracle
+import os
+
+#Configurações de conexão
+DIALECT = 'oracle'
+SQL_DRIVER = 'cx_oracle'
+
+#Pegando as credenciais via variável de ambiente
+
+USERNAME = os.getenv('DB_USER')
+PASSWORD = os.getenv('DB_PASSWORD')
+HOST = os.getenv('DB_HOST')
+PORT = os.getenv('DB_PORT')
+SERVICE = os.getenv('DB_SERVICE')
+
+#Criando a string de conexão
+
+connstr = f"{USERNAME}/{PASSWORD}@{HOST}:{PORT}/{SERVICE}"
+
+#Conectando ao SGBD
+conn = cx_Oracle.connect(connstr)
+
+#Criando um cursor
+cursor = conn.cursor()
+
+#Rodando uma Query para teste
+cursor.execute("select * from global_name")
+result = cursor.fetchall()
+for row in result:
+    print(result)
